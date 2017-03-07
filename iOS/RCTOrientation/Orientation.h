e6e0c8a1322cf1e2aabcf3873fb2740917d220df
mac2066 <j2066@qq.com>
2017-03-08 01:09:19 +0800

    Update Orientation.h
    Inside the react-native, after installing this plugin, you can't find RCTBridgeModule.h at compile time
    Need to
    #import "RCTBridgeModule.h"
    Modified into
    #import <React/RCTBridgeModule.h>

--- iOS/RCTOrientation/Orientation.h
+++ iOS/RCTOrientation/Orientation.h
@@ -4,7 +4,8 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "RCTBridgeModule.h"
+//#import "RCTBridgeModule.h"
+#import <React/RCTBridgeModule.h>
 
 @interface Orientation : NSObject <RCTBridgeModule>
 + (void)setOrientation: (UIInterfaceOrientationMask)orientation;
